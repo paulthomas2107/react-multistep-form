15f5fff1cfbbb5cc2ef69586f9990cbcc80120a6
Paul Thomas <paulthomas2107@gmail.com>
2022-10-05 14:46:14 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import { AccountForm } from './AccountForm';
+import { AddressForm } from './AddressForm';
+import { useMultiStepForm } from './useMultiStepForm';
+import { UserForm } from './UserForm';
+
+function App() {
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
+    useMultiStepForm([<UserForm />, <AddressForm />, <AccountForm />]);
+  return (
+    <div
+      style={{
+        position: 'relative',
+        background: 'white',
+        border: '1px solid black',
+        padding: '2rem',
+        margin: '1rem',
+        borderRadius: '.5rem',
+        fontFamily: 'Arial',
+      }}
+    >
+      <form>
+        <div
+          style={{
+            position: 'absolute',
+            top: '.5rem',
+            right: '.5rem',
+          }}
+        >
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div
+          style={{
+            margin: '1rem',
+            display: 'flex',
+            gap: '.5rem',
+            justifyContent: 'flex-end',
+          }}
+        >
+          {!isFirstStep && (
+            <button type="button" onClick={back}>
+              Back
+            </button>
+          )}
+          <button type="button" onClick={next}>
+            {isLastStep ? 'Finish' : 'Next'}
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FormWrapper.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from 'react';
+
+type FormWrapperProps = {
+  title: string;
+  children: ReactNode;
+};
+
+export function FormWrapper({ title, children }: FormWrapperProps) {}
